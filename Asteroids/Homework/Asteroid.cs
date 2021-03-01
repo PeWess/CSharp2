94443d9cb6adef10e450fbb11c87d04225ce4232
Sergey K <adv.sk2521@yandex.ru>
2021-03-01 20:49:16 +0300

    Lesson-1
    Uploaded a homework for lesson 1

---
+++ Asteroids/Homework/Asteroid.cs
@@ -0,0 +1,38 @@
+﻿using Homework.Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    public class Asteroid
+    {
+        protected Point Position;
+        protected Point Direction;
+        protected Size Size;
+
+        public Asteroid(Point pos, Point dir, Size size)
+        {
+            Position = pos;
+            Direction = dir;
+            Size = size;
+        }
+
+        public virtual void Draw(Bitmap aster)
+        {
+            Game.Buffer.Graphics.DrawImage(aster, Position.X, Position.Y, Size.Width, Size.Height);
+        }
+
+        public virtual void Update()
+        {
+            Position.X = Position.X + Direction.X;
+            Position.Y = Position.Y + Direction.Y;
+
+            if (Position.X < 0 || Position.X > Game.Width) Direction.X *= -1;
+            if (Position.Y < 0 || Position.Y > Game.Height) Direction.Y *= -1;
+        }
+    }
+}
---
+++ Asteroids/Homework/Star.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Star : Planet
+    {
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size) { }
+
+        public void Draw()
+        {
+            Game.Buffer.Graphics.DrawRectangle(Pens.White, Position.X, Position.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Position.X = Position.X + Direction.X;
+            if (Position.X < 0)
+            {
+                Position.X = Game.Width;
+                Position.Y = new Random().Next(0, Game.Height);
+            }
+        }
+    }
+}
