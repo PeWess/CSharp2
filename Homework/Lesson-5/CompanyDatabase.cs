bf5fa03741069d50ace9af775c3da19e1e517cd7
Sergey K <adv.sk2521@yandex.ru>
2021-03-29 18:22:32 +0300

    Lesson-6
    Upload a Homework for lesson 6

---
+++ Homework/Lesson-5/CompanyDatabase.cs
@@ -0,0 +1,66 @@
+﻿using Company.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    public class CompanyDatabase
+    {
+        Random rnd = new Random();
+        string filename = AppDomain.CurrentDomain.BaseDirectory + "Names.txt";
+
+        public List<string> departments = new List<string>{
+        "Information Security",
+        "IT Support",
+        "IT Development",
+        "Software Development",
+        "Quality Control"};
+
+        public List<string> countries = new List<string>{
+        "Россия",
+        "Германия",
+        "Швеция",
+        "Китай",
+        "США"};
+
+        public ObservableCollection<Employee> Employees{ get; set; }
+        public ObservableCollection<Department> Departments { get; set; }
+
+        public CompanyDatabase()
+        {
+            Employees = new ObservableCollection<Employee>();
+            Departments = new ObservableCollection<Department>();
+            GenerateCompanyList();
+        }
+
+
+        public void GenerateCompanyList()
+        {
+            StreamReader sr = new StreamReader(filename);
+
+            for(int i = 0; i < 5; i++)
+            {
+                string department = departments[i];
+                string country = countries[i];
+                Departments.Add(new Department(department, country));
+            }
+
+            while(!sr.EndOfStream)
+            {
+                string[] FIO = sr.ReadLine().Split(' ');
+                string surname = FIO[0];
+                string name = FIO[1];
+                string patronymic = FIO[2];
+                bool trainee = rnd.Next(0, 2) == 0 ? false : true;
+                Department department = Departments[rnd.Next(0, Departments.Count)];
+                Employees.Add(new Employee(name, surname, patronymic, trainee, department));
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Homework/Lesson-5/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using Company.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static CompanyDatabase database = new CompanyDatabase();
+
+        public ObservableCollection<Employee> EmployeesList { get; set; }
+
+        public Employee SelectedEmployee { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            EmployeesList = database.Employees;
+        }
+
+        private void CompanyWorkersListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count != 0)
+            {
+                workerControl.Employee = (Employee)SelectedEmployee.Clone();
+            }
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (CompanyWorkersListView.SelectedItems.Count == 0) return;
+
+            if (CompanyWorkersListView.SelectedItems.Count != 0)
+            {
+                EmployeesList[EmployeesList.IndexOf(SelectedEmployee)] = workerControl.Employee;
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (CompanyWorkersListView.SelectedItems.Count == 0) return;
+
+            if(MessageBox.Show("Вы точно хотите удалить данного сотрудника?", "Внесение изменений в базу данных", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                database.Employees.Remove((Employee)CompanyWorkersListView.SelectedItems[0]);
+            }
+        }
+
+        private void btnAddDepartment_Click(object sender, RoutedEventArgs e)
+        {
+            AddDepartment adder = new AddDepartment();
+            adder.ShowDialog();
+        }
+
+        private void btnAddWorker_Click(object sender, RoutedEventArgs e)
+        {
+            AddWorker adder = new AddWorker();
+            adder.ShowDialog();
+        }
+    }
+}
