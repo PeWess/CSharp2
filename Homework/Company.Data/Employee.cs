a0262db202b93c5a04d73d51cd49a92a36ff5848
Sergey K <adv.sk2521@yandex.ru>
2021-04-05 18:36:37 +0300

    Lesson-8
    Uploaded a homework for lesson 8

---
+++ Homework/Company.Data/Employee.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Data
+{
+    public class Employee : INotifyPropertyChanged, ICloneable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _ID;
+
+        private string _name;
+
+        private string _surname;
+
+        private string _patronymic;
+
+        private bool _trainee;
+
+        private string[] _curDep;
+
+        public int ID { get; set; }
+
+        public string Name
+        {
+            get { return _name; }
+
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Surname
+        {
+            get { return _surname; }
+
+            set
+            {
+                _surname = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public string Patronymic
+        {
+            get { return _patronymic; }
+
+            set
+            {
+                _patronymic = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public bool Trainee
+        {
+            get { return _trainee; }
+
+            set
+            {
+                _trainee = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string[] CurDep
+        {
+            get { return _curDep; }
+
+            set
+            {
+                _curDep = value;
+                NotifyPropertyChanged();
+            }
+        }
+        public string FIO
+        {
+            get
+            {
+                return Surname + " " + Name + " " +  Patronymic;
+            }
+        }
+
+        public string PlaceOfWork
+        {
+            get
+            {
+                return CurDep[0] + "-" + CurDep[1];
+            }
+
+            set
+            {
+                if (value != "")
+                {
+                    string[] str = value.Split('-');
+                    CurDep[0] = str[0];
+                    CurDep[1] = str[1];
+                }
+            }
+        }
+
+        public Employee() 
+        {
+            CurDep = new string[2];
+        }
+
+        public Employee(int id, string name, string surname, string patronymic, bool trainee, Department dep)
+        {
+            ID = id;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Trainee = trainee;
+            CurDep = new string[2];
+            CurDep[0] = dep.Dep;
+            CurDep[1] = dep.Country;
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ Homework/Lesson-5/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using CompanyCommunication.CompanyService;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lesson_5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static CompanyDatabase database = new CompanyDatabase();
+
+        public ObservableCollection<Employee> EmployeesList { get; set; }
+
+        public Employee SelectedEmployee { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            EmployeesList = database.Employees;
+        }
+
+        private void CompanyWorkersListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(e.AddedItems.Count != 0)
+            {
+                workerControl.Employee = (Employee)SelectedEmployee.Clone();
+            }
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (CompanyWorkersListView.SelectedItems.Count == 0) return;
+
+            if (database.Update(workerControl.Employee) > 0)
+            {
+                EmployeesList[EmployeesList.IndexOf(SelectedEmployee)] = workerControl.Employee;
+                MessageBox.Show("Данный о сотруднике успешно изменены", "Внесение изменений в базу данных", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (CompanyWorkersListView.SelectedItems.Count == 0) return;
+
+            if(MessageBox.Show("Вы точно хотите удалить данного сотрудника?", "Внесение изменений в базу данных", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                if(database.Remove((Employee)CompanyWorkersListView.SelectedItems[0]) > 0)
+                {
+                    MessageBox.Show("Сотрудник успешно удален", "Внесение изменений в базу данных", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+        }
+
+        private void btnAddDepartment_Click(object sender, RoutedEventArgs e)
+        {
+            AddDepartment adder = new AddDepartment();
+            adder.ShowDialog();
+        }
+
+        private void btnAddWorker_Click(object sender, RoutedEventArgs e)
+        {
+            AddWorker adder = new AddWorker();
+            adder.ShowDialog();
+        }
+    }
+}
